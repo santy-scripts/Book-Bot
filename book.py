c17e63b7d398c9880af97b3f62cc640aa324d6d8
santy <sandresrodriguezmo@gmail.com>
2025-08-14 16:13:06 +0200

    working fine with input and adding ASCII for enhancements

--- book.py
+++ book.py
@@ -1,35 +1,40 @@
-phrase = input("ingrese una frase: ")
+print("""    
+__________               __               __________ ___________________
+\______   \ ____   ____ |  | __           \______   \\_____  \__    ___/
+ |    |  _//  _ \ /  _ \|  |/ /   ______   |    |  _/ /   |   \|    |   
+ |    |   (  <_> |  <_> )    <   /_____/   |    |   \/    |    \    |   
+ |______  /\____/ \____/|__|_ \            |______  /\_______  /____|   
+        \/                   \/                   \/         \/         
+      """)
+
+phrase = input("Ingrese una frase: ")
 word = phrase.split(" ")
 abecedario = [
     "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
     "n", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
 ]
 
-# script
 def book():
-
-    contador = 0
-    contadorABC = 0
+    total_letras = 0
+    contadorABC = {abc: 0 for abc in abecedario}  
 
     for palabra in word:
-
-        if word != " ":
-            contador += len(palabra)
-
-        totalLetras = print(f"total de letras encontradas", contador)
-
-        for letra in palabra:
-
-            for abc in abecedario:
-
-                if abc == letra:
-                    contadorABC += 1
-
-                totalABC = print(f"{abc}: {contadorABC}")
-
-    totalPalabras = print(f"total de palabras encontradas es {len(word)}")
-
-    return totalABC, totalLetras, totalPalabras
-
+        if palabra != "":
+            total_letras += len(palabra)
+            for letra in palabra.lower():
+                if letra in contadorABC:
+                    contadorABC[letra] += 1 
+                    
+                    
+    print("============ BOOKBOT ============")
+    print(f"----------- Word Count ----------")
+    print(f"{len(word)}")
+    print(f"--------- Character Count -------")
+    print(f"{total_letras}")
+    print(f"-------- Count per Letter -------")
+    for letra, cuenta in contadorABC.items(): 
+        print(f"{letra}: {cuenta}")
+    print("============= END ===============")
 
 book()
+
